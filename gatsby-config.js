2cc6bb1088804f10fd50f91ee12591d337c5d4bf
yuekaim <yuekaim@gmail.com>
2023-02-19 02:02:18 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `rundong`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms"]
+};
